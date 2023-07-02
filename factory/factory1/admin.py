c976bf1fa1ebb34ff00d568fed5bc64e9b09ddae
Adenidays <133434653+Adenidays@users.noreply.github.com>
2023-07-02 03:00:53 +0300

    Initial commit

---
+++ factory/factory1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from factory1.models import ConfectioneryFactoryproduct1
+
+
+admin.site.register(ConfectioneryFactoryproduct1)
---
+++ factory/factory2/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from factory2.models import ConfectioneryFactoryproduct2
+# Register your models here.
+admin.site.register(ConfectioneryFactoryproduct2)
